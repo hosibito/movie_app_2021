e2741a04f9e5acab8aee730c0a8d463715ba1752
hosibito <hosibito@naver.com>
2022-03-09 11:56:06 +0900

    7.0 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi :)");   // 처음 실행될때 실행
-    return () => console.log("bye :(");  // 실행이 종료될때 실행
-  }, []);
-
-  return <h1>Hello</h1>;
-}
-
-function Hello2(){  // 위와 같다. 
-  function byFn() {
-    console.log("bye :(");
-  }
-  function hiFn(){
-    console.log("hi :)"); 
-    return byFn;
-  }
-  useEffect(hiFn, []);
-
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]); 
+    // 자마스크립트 문법. 기존리스트와 새 요소를 더한 새 리스트를 작성
+    // 리엑트에서는 절대로 toDos.push(toDo) 같이 직접 수정해서는 안된다. 
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input  
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
@@ -36,8 +33,36 @@ function App() {
 export default App;
 
 /*
-  show hide 를 누를때마다
-  Hello() 의 hi 와 bye 가 계속나타남.
-  이유는 {showing ? <Hello /> : null} 에의해 
-  사라지고(디스트로이) 나타나기(크리에이트)를 반복하기 떄문이다. 
+ input 만드는 의식의 흐름.
+    1. input 를 만든다.
+      <input type="text" placeholder="Write your to do..."/>
+    2. input 에서 입력될 값을 다룰 공간을 마련한다. 
+      const [toDo, setToDo] = useState("");  기본값을 정해주고, 받아올value 값과 수정할수 있는 함수를 지정.
+    3. input에 value를 연결한다. 
+      <input value={toDo} type="text" placeholder="Write your to do..."/>
+    4. input 에 이벤트를 연결한다.(인풋 내용이 바뀔떄)
+      <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."/>
+    5. 이벤트발생시 처리내용을 작성한다. 
+      const onChange = (event) => setToDo(event.target.value);     
+    6. 완성.
+
+  form 만드는 의식의 흐름
+    1. form 을 만들고 submit 버튼 추가
+      <form><button>Add To Do</button></form>
+    2. sebmit 눌렸을때의 동작을 이벤트에 연결
+      <form onSubmit={onSubmit}>
+    3. 이벤트를 작성.
+      const onSubmit = (event) => {  
+        event.preventDefault(); 
+        console.log(toDo)  // 위에서 만들어진 값은 사용할수 있다. 
+      }
+    4. 이벤트 완성.
+      const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+          return;
+        }
+        setToDos((currentArray) => [toDo, ...currentArray]);
+        setToDo("");
+      };
 */
