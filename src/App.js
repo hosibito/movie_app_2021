3397e6f51da18dc8daef3c2fb28e8d9b9f966b98
hosibito <hosibito@naver.com>
2022-03-14 20:34:17 +0900

    7.5 React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Routes>       
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Detail />} />    
+        <Route path="/hello" element={<h1>Hello</h1>} />     
+      </Routes>
+    </BrowserRouter>  
+  );
 }
 export default App;
 
 /*
-  라우터를 이용 페이지 단위 로 표기하기 위한 준비 
+  라우터를 이용 페이지 단위 로 표기한다 
+  공식문서 참고할것.
+
+  루트부터 각 주소에 딸린 컨포넌트들을 표시해 줄수 있다. 
 */
